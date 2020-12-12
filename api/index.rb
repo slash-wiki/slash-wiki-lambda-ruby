7f795aa2ba08461d0e29c838e0a726013e73f3cb
Luna Graysen <75589876+they-them@users.noreply.github.com>
2020-12-12 18:56:27 -0500

    Add implementation

--- api/index.rb
+++ api/index.rb
@@ -1,5 +1,7 @@
+require 'cgi'
+
 Handler = Proc.new do |request, response|
     response.status = 200
     response['Content-Type'] = 'application/json'
-    response.body = "{\"response_type\": \"in_channel\", \"text\": \"Hello World\"}".to_json
+    response.body = JSON[response_type: 'in_channel', text: "Hello #{URI.decode_www_form(request.body).to_h['user_id']}, the date and time is #{Time.new}."]
 end
