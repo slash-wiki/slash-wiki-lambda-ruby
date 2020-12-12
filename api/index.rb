d08334fe73b4e4fca18a03db258e5f9a3cd9c26d
Luna <75589876+they-them@users.noreply.github.com>
2020-12-12 18:53:13 -0500

    attempt

--- api/index.rb
+++ api/index.rb
@@ -3,5 +3,5 @@ require 'cgi'
 Handler = Proc.new do |request, response|
     response.status = 200
     response['Content-Type'] = 'application/json'
-    response.body = JSON[response_type: 'in_channel', text: "Hello #{CGI.unescape(request.body)}, the date and time is #{Time.new}."]
+    response.body = JSON[response_type: 'in_channel', text: "Hello #{URI.decode_www_form(request.body).to_h['user_id']}, the date and time is #{Time.new}."]
 end
