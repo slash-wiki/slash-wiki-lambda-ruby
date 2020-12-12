c9a0000df44cd639b08e89dc4baa3b75d2ae8230
Luna <75589876+they-them@users.noreply.github.com>
2020-12-12 16:22:56 -0500

    Add implementation

--- api/index.rb
+++ api/index.rb
@@ -1,5 +1,5 @@
 Handler = Proc.new do |request, response|
     response.status = 200
     response['Content-Type'] = 'application/json'
-    response.body = "{\"response_type\": \"in_channel\", \"text\": \"Hello World\"}".to_json
+    response.body = "{\"response_type\": \"in_channel\", \"text\": \"Hello #{request.method(:user_name)}, the date and time is #{Time.new}.\"}".to_json
 end
