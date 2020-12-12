1b2a3b76c585f9a25770a8a1af7f42d099a3167d
Luna <75589876+they-them@users.noreply.github.com>
2020-12-12 17:01:05 -0500

    testing something

--- api/index.rb
+++ api/index.rb
@@ -3,5 +3,5 @@ require 'uri'
 Handler = Proc.new do |request, response|
     response.status = 200
     response['Content-Type'] = 'application/json'
-    response.body = JSON[response_type: 'in_channel', text: "Hello #{URI.decode(request.body).user_id}, the date and time is #{Time.new}."]
+    response.body = JSON[response_type: 'in_channel', text: "Hello #{URI.decode(request.body)}, the date and time is #{Time.new}."]
 end
